cbfa710050b7aef45924f6966f565f7a59b44560
shaozelin <1327914853@qq.com>
2023-02-20 16:15:06 +0800

    feat: init

---
+++ portal-used-plugin/readme.md
@@ -0,0 +1,9 @@
+### step
+1. get the dependency map(path) according to the entry point (src/index)
+2. find the portal function
+3. according to function we gonna find where used, dfs in the map
+4. find where the function is used(file, col, row)
+
+### extra in plug
+1. can input portal
+2. can input include and exclude
---
+++ portal-used-plugin/src/plugin.js
@@ -0,0 +1,19 @@
+const { declare } = require('@babel/helper-plugin-utils');
+
+const customedPlugin = declare((api, options, dirname) => {
+  api.assertVersion(7)
+
+  return {
+    pre(file) {
+      file.set('state', []);
+    },
+    visitor: {
+      Program: {
+        enter(path, state) {},
+      },
+      'VariableDeclaration|ReturnStatement'(path, state) {},
+    }
+  }
+})
+
+module.exports = customedPlugin
